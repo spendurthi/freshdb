17ee81c67bd292efbcd250e8ca6e5d9d55fb671b
pendurthis <pendurthis@SuryaPC>
2016-12-25 22:04:28 -0500

    3

--- src/main/java/com/sone/freshdb/dao/DeptDAO.java
+++ src/main/java/com/sone/freshdb/dao/DeptDAO.java
@@ -3,9 +3,11 @@ package com.sone.freshdb.dao;
 import java.util.List;
 
 import com.sone.freshdb.dto.DeptDTO;
+import com.sone.freshdb.exceptions.FreshDbException;
 
 public interface DeptDAO {
 
-	void save(DeptDTO dto);
-	List<DeptDTO> getAllDepts();
+	void save(DeptDTO dto) throws FreshDbException;
+	List<DeptDTO> getAllDepts()throws FreshDbException;
+	List<DeptDTO> getDepts(DeptDTO deptDto)throws FreshDbException;
 }
